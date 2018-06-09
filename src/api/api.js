1a1cb203f437b9bf909821fcb4f92971fae4c93a
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-06-09 14:11:32 -0700

    added post and put tests work in httpie

--- src/api/api.js
+++ src/api/api.js
@@ -19,13 +19,18 @@ router.get('/', (req,res) => {
 
 router.get('/api/v1/dogs', (req, res) => {
   const id = req.query.id;
-  if(id) {
+  if(id == 1976) {
     res.statusCode = 200;
     let msg = `ID: ${req.query.id}`;
     res.write(msg);
+  } else if(id) {
+    res.statusCode = 404;
+    res.statusMessage = 'not found';
   } else {
-    res.statusCode - 400;
+    res.statusCode = 400;
     res.statusMessage = 'bad request';
+    console.log(res.statusMessage);
+    //res.write((JSON.stringify({error: 'bad request'})));
   }
   res.end();
 });
@@ -36,10 +41,31 @@ router.get('/api/v1/dogs', (req, res) => {
  *     echo '{"title":"Go Home","content":"foobar"}' | http post http://localhost:8080/data
  */
 router.post('/data', (req,res) => {
+  console.log('here in post');
   res.statusCode = 200;
   res.statusMessage = 'OK';
-  //req.body = '{"title":"Go Home","content":"foobar"}';
-  res.write( JSON.stringify(req.body) );
+  if(req.body) {
+    res.write( JSON.stringify(req.body));
+  } else {
+    res.statusCode = 400;
+  }
+  res.end();
+});
+
+router.put('/update', (req, res) => {
+  const id = req.query.id;
+  if(id) {
+    console.log('here in PUT');
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    let msg = '{"ID": "`${req.query.id}`", "content": "PUT to the test"}';
+    res.write(JSON.stringify(msg));
+  } else {
+    console.log('here in PUT error');
+    res.statusCode = 400;
+    res.statusMessage = 'bad request';
+    console.log(res.statusMessage);
+  }
   res.end();
 });
 
